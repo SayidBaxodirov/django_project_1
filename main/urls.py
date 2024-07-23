4eb402ebdb481e0a9962a2169e54ca9bfdd0905c
Sayidabdullakhon <20221500615@newuzbekistanuniversity.uz>
2024-07-23 11:31:29 +0500

    other pages added

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import main_page
+from .views import main_page, contact,show_contacts
 
 urlpatterns = [
-    path("", main_page, name="main_page")
+    path("", main_page, name="main_page"),
+    path("contact/", contact, name="contact"),
+    path("contacts/",show_contacts,name="show_contacts")
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,26 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+
+from .forms import ContactForm
+from .models import Contact
 
 
 # Create your views here.
 def main_page(request):
     return render(request, 'main/index.html')
+
+
+def contact(request):
+    form = ContactForm(request.POST)
+    if request.POST and form.is_valid():
+        form.save()
+        return redirect('show_contacts')
+    ctx = {'form': form}
+    return render(request, 'main/contact.html',ctx)
+
+
+
+def show_contacts(request):
+    contacts = Contact.objects.all()
+    ctx = {'contacts': contacts}
+    return render(request, 'main/all_contacts.html',ctx)
